693de022cf5f98a64a5217958431b3e8f3e83a97
Arthur Bonavita <arthur.bonavita@gmail.com>
2017-09-30 01:39:21 +0200

    basic form format

--- index.php
+++ index.php
@@ -7,15 +7,46 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   </head>
   <body>
+    <div class="container-fluid">
+      <div class="jumbotron text-center">
+        <h1>Email Form</h1>
+      </div>
+      <div class="">
+        <form method="post">
+            <div class="form-group">
+              <label for="name">Your Name:</label>
+              <input type="text" name="name" class="form-control" placeholder="John">
+            </div>
+            <div class="form-group">
+              <label for="email">Your Email:</label>
+              <input type="email" name="email" class="form-control" placeholder="john@something.com">
+            </div>
+            <div class="form-group">
+              <label for="name">Your Comment:</label>
+              <textarea name="comment" class="form-control"></textarea>
+            </div>
 
+            <input class="btn btn-success" type="submit">
+
+        </form>
+      </div>
+    </div>
+
+
+    </div>
+
+
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
 
 
 <?php
 
+$name="Arthur";
+$surname="Bonavita";
 
-echo "Hello World";
+
 
 
  ?>
