97c11f2fa0a102269493312b0f67008690eecea0
Arthur Bonavita <arthur.bonavita@gmail.com>
2017-09-30 03:15:47 +0200

    add the php functions

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+
+  if($_POST["submit"]) {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(!$_POST['name']) {
+      $error = '<br>Please enter your <strong>name</strong>';
+    }
+    if(!$_POST['email']) {
+      $error .= '<br>Please enter your <strong>email</strong>';
+    }
+    if(!$_POST['message']) {
+      $error .= '<br>Please enter your <strong>message</strong>';
+    }
+    if($_POST['email']!='' AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $error .= "<br> Please enter a <strong>valid email address</strong>";
+    }
+    if($error) {
+    $result='<div class="alert alert-danger">There were error(s) in your form:'.$error.'</div>';
+  } else {
+    if(mail("arthur.bonavita@gmail.com","Message from website", "Name: " . $name)) {
+      $result='<div class="alert alert-success">Thank you! I will be in touch!</div>';
+
+    } else {
+      $result='<div class="alert alert-danger">Sorry, there was an error with your message. Try again later.</div>';
+
+    }
+
+  }
+
+  }
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,23 +47,27 @@
     <div class="container-fluid">
       <div class="jumbotron text-center">
         <h1>Email Form</h1>
+        <p class="lead">Let me an message, I'll respond ASAP.</p>
+        <?php
+        echo $result;
+         ?>
       </div>
-      <div class="">
+      <div class="col-md-6 col-md-offset-3">
         <form method="post">
             <div class="form-group">
               <label for="name">Your Name:</label>
-              <input type="text" name="name" class="form-control" placeholder="John">
+              <input type="text" name="name" class="form-control" placeholder="Lisa">
             </div>
             <div class="form-group">
               <label for="email">Your Email:</label>
-              <input type="email" name="email" class="form-control" placeholder="john@something.com">
+              <input type="email" name="email" class="form-control" placeholder="lisa@gmail.com">
             </div>
             <div class="form-group">
-              <label for="name">Your Comment:</label>
-              <textarea name="comment" class="form-control"></textarea>
+              <label for="name">Your Message:</label>
+              <textarea name="message" class="form-control" placeholder="I loved your email form!!! ;D"></textarea>
             </div>
 
-            <input class="btn btn-success" type="submit">
+            <input class="btn btn-success" name="submit" type="submit">
 
         </form>
       </div>
@@ -39,14 +80,3 @@
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
-
-
-<?php
-
-$name="Arthur";
-$surname="Bonavita";
-
-
-
-
- ?>
